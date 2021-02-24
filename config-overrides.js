b75b45d45e72defd7d181b637456c35add0f5039
Gervin <gervin.sean@weekendinc.com>
2021-02-24 14:41:08 +0700

    removed config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,9 @@
-module.exports = function override(config, env) {
-  config.optimization.runtimeChunk = false;
-  config.optimization.splitChunks = {
-    cacheGroups: {
-      default: false,
-    },
-  };
-  return config;
-};
+// module.exports = function override(config, env) {
+//   config.optimization.runtimeChunk = false;
+//   config.optimization.splitChunks = {
+//     cacheGroups: {
+//       default: false,
+//     },
+//   };
+//   return config;
+// };
