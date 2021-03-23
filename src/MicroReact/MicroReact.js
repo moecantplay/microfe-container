05f9bd17afb9a90682d3993973c4cf08e7e38316
Gervin <gervin.sean@weekendinc.com>
2021-03-23 18:00:18 +0700

    edit MicroReact.js

--- src/MicroReact/MicroReact.js
+++ src/MicroReact/MicroReact.js
@@ -6,7 +6,26 @@ const MicroReact = ({ history, name, host }) => {
   const renderMicroReact = () =>
     window[`render${name}`](`${name}-container`, history);
 
-  const fetchScript = () => {
+  // const fetchScript = () => {
+  //   fetch(`${host}/asset-manifest.json`)
+  //     .then(res => res.json())
+  //     .then(manifest => {
+  //       const script = document.createElement('script');
+  //       script.id = scriptId;
+  //       script.crossOrigin = '';
+  //       script.src = `${host}${manifest['main.js']}`;
+  //       script.onload = renderMicroReact;
+  //       document.head.appendChild(script);
+  //     });
+  // };
+
+  useEffect(() => {
+    if (document.getElementById(scriptId)) {
+      renderMicroReact();
+      return;
+    }
+
+    // fetchScript();
     fetch(`${host}/asset-manifest.json`)
       .then(res => res.json())
       .then(manifest => {
@@ -17,15 +36,6 @@ const MicroReact = ({ history, name, host }) => {
         script.onload = renderMicroReact;
         document.head.appendChild(script);
       });
-  };
-
-  useEffect(() => {
-    if (document.getElementById(scriptId)) {
-      renderMicroReact();
-      return;
-    }
-
-    fetchScript();
   }, []);
 
   useEffect(() => {
